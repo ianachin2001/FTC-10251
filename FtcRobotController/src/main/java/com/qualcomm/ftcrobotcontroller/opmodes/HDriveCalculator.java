2a25fbcd83595e62b54851a87f73ec0b581050eb
hrcomazing <hrcomazing@comcast.net>
2016-08-31 19:42:07 -0400

    Revert "Just Just"
    This reverts commit 2da0a0b4be5363bc0b1c490c3fbcb9d6c540e38d.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/HDriveCalculator.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/HDriveCalculator.java
@@ -3,8 +3,6 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 /**
  * Created by ian on 8/18/2016.
  */
-
-//test comment Luke ... ...
 //Github test comment
     //hirsh ramani is so cool
     //Github test comment 2
