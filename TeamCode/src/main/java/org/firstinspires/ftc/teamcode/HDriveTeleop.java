67c9a1051e50411a35d13e36eb4c5c6aa4bff31a
Luke Darcy <luke_darcy@yahoo.com>
2016-10-16 18:06:56 -0400

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDriveTeleop.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
+import android.util.Log;
+
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.exception.RobotCoreException;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -12,6 +15,7 @@ import com.qualcomm.robotcore.hardware.GyroSensor;
  */
 @TeleOp(name= "HDriveTeleop")
 public class HDriveTeleop extends OpMode {
+    //double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2];
     public GyroSensor gyro2;
     public ModernRoboticsI2cGyro gyro;
     public double gyroAngle;
@@ -19,10 +23,23 @@ public class HDriveTeleop extends OpMode {
     DcMotor rightMotor;
     DcMotor middleMotor;
     HDrive2 calculator;
+    //AdafruitIMU boschBNO055;
     public HDriveTeleop(){
 
     }
     public void init(){
+        /*try {
+            boschBNO055 = new AdafruitIMU(hardwareMap, "bno055"
+
+                    //The following was required when the definition of the "I2cDevice" class was incomplete.
+                    //, "cdim", 5
+
+                    , (byte)(AdafruitIMU.BNO055_ADDRESS_A * 2)//By convention the FTC SDK always does 8-bit I2C bus
+                    //addressing
+                    , (byte)AdafruitIMU.OPERATION_MODE_IMU);
+        } catch (RobotCoreException e){
+            Log.i("FtcRobotController", "Exception: " + e.getMessage());
+        }*/
         gyro2 = hardwareMap.gyroSensor.get("gyro");
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
@@ -52,6 +69,19 @@ public class HDriveTeleop extends OpMode {
         leftMotor.setPower(calculator.getLeftDrive());
         rightMotor.setPower(calculator.getRightDrive());
         middleMotor.setPower(calculator.getMiddleDrive());
+
+        /*boschBNO055.getIMUGyroAngles(rollAngle, pitchAngle, yawAngle);
+		/*
+		 * Send whatever telemetry data you want back to driver station.
+		 */
+        //telemetry.addData("Text", "*** Robot Data***");
+        /*telemetry.addData("Headings(yaw): ",
+                String.format("Euler= %4.5f, Quaternion calculated= %4.5f", yawAngle[0], yawAngle[1]));
+        telemetry.addData("Pitches: ",
+                String.format("Euler= %4.5f, Quaternion calculated= %4.5f", pitchAngle[0], pitchAngle[1]));
+        telemetry.addData("Max I2C read interval: ",
+                String.format("%4.4f ms. Average interval: %4.4f ms.", boschBNO055.maxReadInterval
+                        , boschBNO055.avgReadInterval));*/
 
     }
 }
