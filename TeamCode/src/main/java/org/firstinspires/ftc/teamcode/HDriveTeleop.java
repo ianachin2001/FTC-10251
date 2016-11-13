bf2a6336bdf8e9f9b3ce3ce61218fd43b9d5a238
Luke Darcy <luke_darcy@yahoo.com>
2016-11-13 17:50:29 -0500

    Fixed Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDriveTeleop.java
@@ -1,4 +1,4 @@
-/*package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 import android.util.Log;
 
 import com.qualcomm.hardware.adafruit.BNO055IMU;
@@ -6,12 +6,13 @@ import com.qualcomm.hardware.adafruit.JustLoggingAccelerationIntegrator;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
- import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.qualcomm.robotcore.hardware.DcMotor;
         import com.qualcomm.robotcore.hardware.DcMotorController;
         import com.qualcomm.robotcore.hardware.DcMotorSimple;
         import com.qualcomm.robotcore.hardware.GyroSensor;
         import com.qualcomm.robotcore.hardware.CRServo;
-
+        import com.qualcomm.robotcore.*;
         import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
         import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
         import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -21,56 +22,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
         import java.util.Locale;
 
-/**
- * Created by HIRSH on 8/18/2016.
+
+// * Created by HIRSH on 8/18/2016.
 
 @TeleOp(name= "HDriveTeleop")
 public class HDriveTeleop extends OpMode {
     double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2];
-    public GyroSensor gyro2;
-    public ModernRoboticsI2cGyro gyro;
-//double[] rollAngle = new double[2], pit
-// Orientation angles;
-BNO055IMU imu;
+     Orientation angles;
+    BNO055IMU imu;
     String angleDouble = "hi";
     public double gyroAngle;
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor middleMotor;
-    HDrive2 calculator;
-    AdafruitIMU boschBNO055;
-    //HDriveFCCalc calculator;
+    //HDrive2 calculator;
+    HDriveFCCalc calculator;
     CRServo servo;
-
-    //AdafruitIMU boschBNO055;
     public HDriveTeleop(){
 
     }
     public void init(){
-        try {
-            boschBNO055 = new AdafruitIMU(hardwareMap, "bno055");
-       /*try {
-           boschBNO055 = new AdafruitIMU(hardwareMap, "bno055"
-
-                    //The following was required when the definition of the "I2cDevice" class was incomplete.
-                    //, "cdim", 5
-                   //The following was required when the definition of the "I2cDevice" class was incomplete.
-                   //, "cdim", 5
-
-                    , (byte)(AdafruitIMU.BNO055_ADDRESS_A * 2)//By convention the FTC SDK always does 8-bit I2C buschAngle = new double[2], yawAngle = new double[2];
-                                        //addressing
-                    , (byte)AdafruitIMU.OPERATION_MODE_IMU);
-        } catch (RobotCoreException e){
-            Log.i("FtcRobotController", "Exception: " + e.getMessage());
-        }
-        gyro2 = hardwareMap.gyroSensor.get("gyro");
-                   , (byte)(AdafruitIMU.BNO055_ADDRESS_A * 2)//By convention the FTC SDK always does 8-bit I2C bus
-                   //addressing
-                   , (byte)AdafruitIMU.OPERATION_MODE_IMU);
-       } catch (RobotCoreException e){
-           Log.i("FtcRobotController", "Exception: " + e.getMessage());
-       }
-                    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
             parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
             parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
             parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
@@ -84,27 +56,11 @@ BNO055IMU imu;
             rightMotor = hardwareMap.dcMotor.get("rightMotor");
             middleMotor = hardwareMap.dcMotor.get("middleMotor");
 
-            calculator = new HDrive2();
+            calculator = new HDriveFCCalc();
             //servo = hardwareMap.crservo.get("servo");
-            //calculator = new HDriveFCCalc();
             leftMotor.setDirection(DcMotor.Direction.REVERSE);
             middleMotor.setDirection(DcMotor.Direction.REVERSE);
-        /*gyro = (ModernRoboticsI2cGyro)gyro2;
-        gyro.calibrate();
-        while(gyro.isCalibrating()){
-            try {
-                Thread.sleep(50);
-            }
-            catch(InterruptedException e){
-                e.printStackTrace();
-            }
-        }
-
-            //  runOpMode();
-
-            //waitForStart();
-            //CRServo servo = this.hardwareMap.crservo.get("servo")
-        }
+    }
 
     public void loop(){
         angles   = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
@@ -113,37 +69,10 @@ BNO055IMU imu;
         float leftY = gamepad1.left_stick_y;
         float rightX = gamepad1.right_stick_x;
         float rightY = gamepad1.right_stick_y;
-        calculator.calculateMovement(leftX, leftY, rightX, gyro.getHeading());
-        leftMotor.setPower(calculator.getLeftDrive());
-        rightMotor.setPower(calculator.getRightDrive());
         calculator.calculateMovement(leftX, leftY, rightX, Double.parseDouble(angleDouble));
         leftMotor.setPower(.7*calculator.getLeftDrive());
         rightMotor.setPower(.7*calculator.getRightDrive());
         middleMotor.setPower(calculator.getMiddleDrive());
-
-        boschBNO055.getIMUGyroAngles(rollAngle, pitchAngle, yawAngle);
-		/*
-		 * Send whatever telemetry data you want back to driver station.
-
-       /*boschBNO055.getIMUGyroAngles(rollAngle, pitchAngle, yawAngle);
-     /*
-      * Send whatever telemetry data you want back to driver station.
-
-        //telemetry.addData("Text", "*** Robot Data***");
-        telemetry.addData("Headings(yaw): ",
-                String.format("Euler= %4.5f, Quaternion calculated= %4.5f", yawAngle[0], yawAngle[1]));
-        telemetry.addData("Pitches: ",
-                String.format("Euler= %4.5f, Quaternion calculated= %4.5f", pitchAngle[0], pitchAngle[1]));
-        telemetry.addData("Max I2C read interval: ",
-                String.format("%4.4f ms. Average interval: %4.4f ms.", boschBNO055.maxReadInterval
-                        , boschBNO055.avgReadInterval));
-       /*telemetry.addData("Headings(yaw): ",
-               String.format("Euler= %4.5f, Quaternion calculated= %4.5f", yawAngle[0], yawAngle[1]));
-       telemetry.addData("Pitches: ",
-               String.format("Euler= %4.5f, Quaternion calculated= %4.5f", pitchAngle[0], pitchAngle[1]));
-       telemetry.addData("Max I2C read interval: ",
-               String.format("%4.4f ms. Average interval: %4.4f ms.", boschBNO055.maxReadInterval
-                       , boschBNO055.avgReadInterval));
 
     }
     String formatAngle(AngleUnit angleUnit, double angle) {
@@ -153,4 +82,3 @@ BNO055IMU imu;
         return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
 }
-*/
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpticalDistanceSensor.java
@@ -0,0 +1,21 @@
+/*package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class OpticalDistanceSensor extends OpMode {
+
+    OpticalDistanceSensor opticalDistanceSensor;
+
+    @Override
+    public void init() {
+        opticalDistanceSensor = hardwareMap.opticalDistanceSensor.get("ODS");
+    }
+
+    @Override
+    public void loop() {
+        double value = opticalDistanceSensor.getLightDetected();
+
+        telemetry.addData("Value", value);
+
+    }
+}*/
