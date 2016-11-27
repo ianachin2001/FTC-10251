2f8644a7ad026828298ba000b543ea8f263ccd8a
DAWANG423 <dwangnh@gmail.com>
2016-11-27 16:38:32 -0500

    hershey mayor ramanen

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.GyroSensor;
@@ -27,209 +28,48 @@ import java.util.Locale;
 /**
  * Created by Luke on 9/25/2016.
  */
-@TeleOp(name= "EncodersGonnaCode", group = "HDrive")
+@TeleOp(name= "Run Until Beacon", group = "HDrive")
 public class AutonomousBlue extends LinearOpMode {
-    Orientation angles;
-    BNO055IMU imu;
-    private ElapsedTime runtime = new ElapsedTime();
-    HardwareHDrive robot   = new HardwareHDrive();   // Use a Pushbot's hardware
-    static final double     COUNTS_PER_MOTOR_REV    = 1040 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4; ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
-    TouchSensor             touchsensor;
-
-
+    AnalogInput distanceSensor;
+    HardwareHDrive robot   = new HardwareHDrive();   // Use a HDrive's hardware
     public void runOpMode() throws InterruptedException {
-        touchsensor = hardwareMap.touchSensor.get("touch_sensor");
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
-        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
-        robot.init(hardwareMap);
-        telemetry.addData("Status", "Resetting Encoders");    //
-        telemetry.update();
-        String angleDouble;
         DcMotor leftMotor;
         DcMotor rightMotor;
         DcMotor middleMotor;
         DcMotorController controller;
         DcMotorController controller2;
-        Double gyroDouble;
-        double initialAngle;
+        AnalogInput distanceSensor;
+
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
         middleMotor = hardwareMap.dcMotor.get("middleMotor");
         controller = hardwareMap.dcMotorController.get("Motor Controller 1");
         controller2 = hardwareMap.dcMotorController.get("Motor Controller 2");
-
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Resetting Encoders");    //
-        telemetry.update();
-        //leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        distanceSensor = hardwareMap.analogInput.get("ODS");
 
         robot.leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.middleMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         idle();
+
         robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         robot.middleMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        telemetry.addData("Path0", "Starting at %7d :%7d",
-                robot.leftMotor.getCurrentPosition(),
-                robot.rightMotor.getCurrentPosition(),
-                robot.middleMotor.getCurrentPosition());
-        telemetry.update();
-
-
         waitForStart();
 
-        //Start of Movement
-
-
-
-
-
-
-
-
-
-        //encoderDrive(DRIVE_SPEED, -12, -12, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-        //encoderDriveMiddle(DRIVE_SPEED, 12, 5.0);
-
-
-
-
-
-
-    }
-
-
-
-    public void encoderDriveMiddle(double speed,double middleInches,
-                                   double timeoutS) throws InterruptedException {
-
-
-
-        int newMiddleTarget;
-
-
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            newMiddleTarget = robot.middleMotor.getCurrentPosition() + (int)((middleInches+17) * COUNTS_PER_INCH);
-
-            robot.middleMotor.setTargetPosition(newMiddleTarget);
-
-
-            // Turn On RUN_TO_POSITION
-            robot.middleMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            robot.middleMotor.setPower(Math.abs(speed));
-
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (robot.middleMotor.isBusy() )) {
-
-                // Display it for the driver.
-                telemetry.addData("Path1",  "Running to %7d", newMiddleTarget);
-                telemetry.addData("Path2",  "Running at %7d",
-                        robot.middleMotor.getCurrentPosition());
-
-                telemetry.update();
-
-                // Allow time for other processes to run.
-                idle();
-            }
-
-            // Stop all motion;
-            robot.middleMotor.setPower(0);
-
-            // Turn off RUN_TO_POSITION+
-            robot.middleMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-
-            //  sleep(250);   // optional pause after each move
+        leftMotor.setPower(.1);
+        rightMotor.setPower(.1);
+        while(distanceSensor.getVoltage() < .5) {
+            distanceSensor.getVoltage();
         }
-    }
-    public void encoderDrive(double speed,//int leftDistance, int rightDistance,
-                             double leftInches, double rightInches,
-                             double timeoutS) throws InterruptedException {
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
 
+        telemetry.addData("Value", distanceSensor.getVoltage());
 
-
-        int newLeftTarget;
-        int newRightTarget;
-
-
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            newLeftTarget = /*robot.leftMotor.getCurrentPosition() + */(int)((leftInches-18) * COUNTS_PER_INCH);
-            newRightTarget = /*robot.rightMotor.getCurrentPosition() + */(int)((rightInches-18) * COUNTS_PER_INCH);
-            robot.leftMotor.setTargetPosition(newLeftTarget);
-            robot.rightMotor.setTargetPosition(newRightTarget);
-            // Turn On RUN_TO_POSITION
-            robot.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            robot.leftMotor.setPower(Math.abs(speed));
-            robot.rightMotor.setPower(Math.abs(speed));
-
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (robot.leftMotor.isBusy() && robot.rightMotor.isBusy() )) {
-
-                // Display it for the driver.
-                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
-                telemetry.addData("Path2",  "Running at %7d :%7d",
-                        robot.leftMotor.getCurrentPosition(),
-                        robot.rightMotor.getCurrentPosition());
-
-                telemetry.update();
-
-                // Allow time for other processes to run.
-                idle();
-            }
-
-            // Stop all motion;
-            robot.leftMotor.setPower(0);
-            robot.rightMotor.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-
-            //  sleep(250);   // optional pause after each move
-        }
-    }
-    String formatAngle(AngleUnit angleUnit, double angle) {
-        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
-    }
-
-    String formatDegrees(double degrees){
-        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
 }
