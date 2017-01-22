97e9b6c0f82bde8cae4b1ce0f342052b1cb4b742
hrcomazing <hrcomazing@comcast.net>
2017-01-22 16:36:03 -0500

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDriveTeleop.java
@@ -8,22 +8,22 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.exception.RobotCoreException;
 import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.DcMotorController;
-        import com.qualcomm.robotcore.hardware.DcMotorSimple;
-        import com.qualcomm.robotcore.hardware.GyroSensor;
-        import com.qualcomm.robotcore.hardware.CRServo;
-        import com.qualcomm.robotcore.*;
-        import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-        import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-        import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-        import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-        import org.firstinspires.ftc.robotcore.external.navigation.Position;
-        import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.*;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-        import java.util.Locale;
+import java.util.Locale;
 
 
 // * Created by definitly not HIRSH as he would mess it up and it would explode on 8/18/2016.
@@ -31,12 +31,11 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 @TeleOp(name= "HDriveTeleop Field Centric")
 public class HDriveTeleop extends OpMode {
     double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2];
-     Orientation angles;
+    Orientation angles;
     BNO055IMU imu;
     String angleDouble = "hi";
     public double gyroAngle;
     boolean speedMode = false;
-    boolean bPressed = false;
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor middleMotor;
@@ -51,166 +50,182 @@ public class HDriveTeleop extends OpMode {
     double armAngle = .5;
     double offset = 0;
     int encoder = 0;
-    boolean bumperPressed;
+    int shootTimer = 6;
+    boolean bumperPressed = false;
     boolean bumperIsPressed = false;
     boolean hulianNotAnH = false;
+    boolean shootTimerDone = false;
     Servo buttonPusher;
-
+    boolean state1;
     public HDriveTeleop(){
 
     }
     public void init(){
 
-            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-            parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-            parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-            parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
-            parameters.loggingEnabled      = true;
-            parameters.loggingTag          = "IMU";
-            parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-            imu = hardwareMap.get(BNO055IMU.class, "imu");
-            imu.initialize(parameters);
-            imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
-            leftMotor = hardwareMap.dcMotor.get("leftMotor");
-            rightMotor = hardwareMap.dcMotor.get("rightMotor");
-            middleMotor = hardwareMap.dcMotor.get("middleMotor");
-            shooter = hardwareMap.dcMotor.get("shooter");
-            //servo2 = hardwareMap.servo.get("servo2");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        middleMotor = hardwareMap.dcMotor.get("middleMotor");
+        shooter = hardwareMap.dcMotor.get("shooter");
+        //servo2 = hardwareMap.servo.get("servo2");
         buttonPusher = hardwareMap.servo.get("servo2");
 
 
 
         calculator = new HDriveFCCalc();
-            //servo = hardwareMap.crservo.get("servo");
-            leftMotor.setDirection(DcMotor.Direction.REVERSE);
-            middleMotor.setDirection(DcMotor.Direction.REVERSE);
+        //servo = hardwareMap.crservo.get("servo");
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        middleMotor.setDirection(DcMotor.Direction.REVERSE);
         //shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void loop(){
-        if(gamepad1.b){
-            bPressed = true;
+        int i = 0;
+        angles   = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
+        angleDouble = formatAngle(angles.angleUnit, angles.firstAngle);
+        float leftX = gamepad1.left_stick_x;
+        float leftY = gamepad1.left_stick_y;
+        float rightX = gamepad1.right_stick_x;
+        float rightY = gamepad1.right_stick_y;
+        float left = gamepad1.left_trigger;
+        float right = gamepad1.right_trigger;
+        float leftTrigger = gamepad2.left_trigger;
+        float rightTrigger = gamepad2.right_trigger;
+        boolean buttonAPressed = gamepad1.a;
+        boolean buttonXPressed = gamepad1.x;
+        boolean buttonAPressed2 = gamepad2.a;
+        boolean buttonXPressed2 = gamepad2.x;
+        //bumperPressed = gamepad1.right_bumper;
+        if(countUp){
+            if(countsinceapressed < 10){
+                countsinceapressed++;
+            }
+            else{
+                countUp = false;
+                countsinceapressed = 0;
+            }
         }
-        else if(gamepad1.a){
-            bPressed = false;
+        if(buttonAPressed&& !countUp) {
+            countUp = true;
+            if (speedMode == false) {
+                speedMode = true;
+                leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                middleMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                telemetry.addData("Mode", "Speed");
+                telemetry.update();
+            } else if (speedMode == true) {
+                speedMode = false;
+                leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                middleMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                telemetry.addData("Mode", "Power");
+                telemetry.update();
+            }
         }
-        if(!bPressed) {
-            int i = 0;
-            angles = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
-            angleDouble = formatAngle(angles.angleUnit, angles.firstAngle);
-            float leftX = gamepad1.left_stick_x;
-            float leftY = gamepad1.left_stick_y;
-            float rightX = gamepad1.right_stick_x;
-            float rightY = gamepad1.right_stick_y;
-            float left = gamepad1.left_trigger;
-            float right = gamepad1.right_trigger;
-            float leftTrigger = gamepad2.left_trigger;
-            float rightTrigger = gamepad2.right_trigger;
-            boolean buttonAPressed = gamepad1.a;
-            boolean buttonXPressed = gamepad1.x;
-            boolean buttonAPressed2 = gamepad2.a;
-            boolean buttonXPressed2 = gamepad2.x;
-            if (countUp) {
-                if (countsinceapressed < 10) {
-                    countsinceapressed++;
-                } else {
-                    countUp = false;
-                    countsinceapressed = 0;
-                }
-            }
-            if (buttonAPressed && !countUp) {
-                countUp = true;
-                if (speedMode == false) {
-                    speedMode = true;
-                    leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                    rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                    middleMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                    shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                    telemetry.addData("Mode", "Speed");
-                    telemetry.update();
-                } else if (speedMode == true) {
-                    speedMode = false;
-                    leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                    rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                    middleMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                    telemetry.addData("Mode", "Power");
-                    telemetry.update();
-                }
-            }
-            // telemetry.addData("Encoder Position", shooter.getCurrentPosition());
-            telemetry.addData("Angle", Double.parseDouble(angleDouble) + offset);
-            telemetry.addData("Left Trigger", gamepad1.left_trigger);
-            telemetry.addData("Left Stick X", gamepad1.left_stick_x);
-            telemetry.addData("Right Stick Y", gamepad1.right_stick_x);
-            telemetry.addData("Left Stick X", gamepad1.left_stick_y);
-            telemetry.addData("Right Stick Y", gamepad1.right_stick_y);
-            telemetry.update();
+        // telemetry.addData("Encoder Position", shooter.getCurrentPosition());
+        // telemetry.addData("Angle", Double.parseDouble(angleDouble)+offset);
+        // telemetry.addData("Left Trigger", gamepad1.left_trigger);
+        // telemetry.addData("Left Stick X" , gamepad1.left_stick_x);
+        //telemetry.addData("Right Stick Y" , gamepad1.right_stick_x);
+        //telemetry.addData("Left Stick X" , gamepad1.left_stick_y);
+        //telemetry.addData("Right Stick Y" , gamepad1.right_stick_y);
+        //telemetry.update();
 
 
-            if (buttonXPressed == true) {
-                offset = Double.parseDouble(angleDouble);
-                offset = -offset;
-            }
-
-
-            calculator.calculateMovement(leftX, leftY, rightX, Double.parseDouble(angleDouble) + offset);
-            if (!speedMode) {
-                leftMotor.setPower(.7 * calculator.getLeftDrive());
-                rightMotor.setPower(.7 * calculator.getRightDrive());
-                middleMotor.setPower(-calculator.getMiddleDrive());
-            } else {
-                leftMotor.setPower(calculator.getLeftDrive());
-                rightMotor.setPower(calculator.getRightDrive());
-                middleMotor.setPower(-calculator.getMiddleDrive());
-            }
-            if (left > 0) {
-                buttonPusher.setPosition(1);
-            }
-            if (right > 0) {
-                buttonPusher.setPosition(0);
-            }
-     /*   if(gamepad1.left_bumper == true) {
-            leftMotor.setPower(-.1);
-            rightMotor.setPower(-.1);
+        if(buttonXPressed == true){
+            offset = Double.parseDouble(angleDouble);
+            offset = -offset;
         }
-        if(gamepad1.right_bumper == true) {
-            leftMotor.setPower(.1);
-            rightMotor.setPower(.1);
-        }*/
-        /*if(gamepad1.right_bumper) {
-            shooter.setTargetPosition(3120);
+
+
+        calculator.calculateMovement(leftX, leftY, rightX, Double.parseDouble(angleDouble)+offset);
+        if(!speedMode) {
+            leftMotor.setPower(.7 * calculator.getLeftDrive());
+            rightMotor.setPower(.7 * calculator.getRightDrive());
+            middleMotor.setPower(-calculator.getMiddleDrive());
+        }
+        else{
+            leftMotor.setPower(calculator.getLeftDrive());
+            rightMotor.setPower(calculator.getRightDrive());
+            middleMotor.setPower(-calculator.getMiddleDrive());
+        }
+        if(left > 0) {
+            buttonPusher.setPosition(1);
+        }
+        if(right > 0) {
+            buttonPusher.setPosition(0);
+        }
+    /*   if(gamepad1.left_bumper == true) {
+           leftMotor.setPower(-.1);
+           rightMotor.setPower(-.1);
+       }
+       if(gamepad1.right_bumper == true) {
+           leftMotor.setPower(.1);
+           rightMotor.setPower(.1);
+<<<<<<< HEAD
+       }*/
+        if(!shootTimerDone){
+            shootTimer++;
+            if(shootTimer > 5){
+                shootTimerDone = true;
+            }
+        }
+        if(gamepad1.left_bumper && shootTimerDone && !bumperIsPressed) {
+            shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            shooter.setTargetPosition(3360);
             shooter.setPower(1);
             shooter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             bumperIsPressed = true;
+            telemetry.addLine(Double.toString(shooter.getCurrentPosition()));
+            telemetry.update();
+            shootTimer = 0;
         }
-        if(shooter.getCurrentPosition() == 3120 && bumperIsPressed) {
+        if(shooter.getCurrentPosition() >= 3359 && shooter.getCurrentPosition() <= 3365 && bumperIsPressed) {
+            shootTimerDone = false;
             shooter.setPower(0);
             bumperIsPressed = false;
             shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            telemetry.addLine(Double.toString(shooter.getCurrentPosition()));
+            telemetry.update();
         }
-        if(gamepad1.left_bumper) {
-            shooter.setTargetPosition(3120);
-            shooter.setPower(1);
-            shooter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            hulianNotAnH = true;
+        telemetry.addLine(Double.toString(shooter.getCurrentPosition()));
+        telemetry.update();
+       /*if(gamepad1.right_bumper) {
+           shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+           shooter.setTargetPosition(3120);
+           shooter.setPower(1);
+           shooter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+           hulianNotAnH = true;
+       }
+       //set to maximum overdrive
+       if(shooter.getCurrentPosition() == 3120 && hulianNotAnH == true) {
+           shooter.setPower(0);
+           hulianNotAnH = false;
+           shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+           shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+       }
+       servo2.setPosition(armAngle);
+       */
+        //**********************************************************************
+        if(gamepad1.right_bumper) {
+            bumperPressed = true;
         }
-        //set to maximum overdrive
-        if(shooter.getCurrentPosition() == 3120 && hulianNotAnH == true) {
-            shooter.setPower(0);
-            hulianNotAnH = false;
-            shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }*/
-
-            servo2.setPosition(armAngle);
-
+        else {
+            bumperPressed = false;
         }
-        else{
-
-        }
+        //***********************************************************************
     }
 
     String formatAngle(AngleUnit angleUnit, double angle) {
@@ -222,3 +237,4 @@ public class HDriveTeleop extends OpMode {
 
     }
 }
+
